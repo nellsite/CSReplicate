ed00d82faec64b915366eee9d1ab19565cd2f01d
nellsite <littleanniesdaddy@gmail.com>
2017-03-20 19:56:32 +0000

    Initial commit

---
+++ CSReplicate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSReplicate.Startup))]
+namespace CSReplicate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
